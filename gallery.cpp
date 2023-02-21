9d60b07fc70f216019672e8236cd4ca2c8f4548f
nVietUK <vietuk.nguyen.git@icloud.com>
2023-02-21 20:41:19 +0700

    update gallery.cpp

--- gallery.cpp
+++ gallery.cpp
@@ -10,27 +10,33 @@ using namespace std;
 #define intf "%d"
 
 struct guard {
-    int start, end, cost, id;
-    guard(int start=-1, int end=-1, int cost=-1, int id=-1) : start(start), end(end), cost(cost), id(id) {};
-    inline bool operator<(guard a) const { return cost>a.cost; }
+    int start, end, cost;
+    guard(int start=-1, int end=-1, int cost=-1) : start(start), end(end), cost(cost) {};
+    inline bool operator<(guard a) const { return start<a.start; }
+    inline bool operator>(guard a) const { return cost>a.cost; }
 };
 
-int solve(vector<guard> guards[], int &n, int &m) {
-    priority_queue<guard> p; for (guard i : guards[0]) p.push(i);
-    bool passed[m+1]; fill(passed, passed+m+1, false);
+int solve(guard guards[], int &n, int &m) {
+    int cost[n+1], cur=1; cost[n]=INT_MAX; cost[0]=0;
+    priority_queue<guard, vector<guard>, greater<guard>> pq;
+    
+    for (int i=1; i<=m; i++) {
+        while (cur<=guards[i].start) {
+            while (pq.top().end<cur) pq.pop();
+            cost[cur]=pq.top().cost; cur++;
+        }
 
-    while (p.size()) {
-        guard cur=p.top(); p.pop();
-
-        if (passed[cur.id]) continue;
-        passed[cur.id]=true;
-        if (cur.end==n) return cur.cost;
-
-        for (int i=cur.start+1; i<=cur.end && i<=n; i++)
-            for (guard ii : guards[i])
-                p.push(guard(ii.start, ii.end, ii.cost+cur.cost, ii.id));
+        pq.push(guard(guards[i].start, guards[i].end, cost[cur-1]+guards[i].cost));
     }
-    return -1;
+
+    int tmp=INT_MAX;
+    while (pq.size()) {
+        if (pq.top().end>=n) {
+            tmp=pq.top().cost; break;
+        }
+        pq.pop();
+    }
+    return min(cost[n], tmp);
 }
 
 signed main() {
@@ -44,10 +50,11 @@ signed main() {
 #endif
 
     int n, m; scanf(intf intf, &n, &m);
-    vector<guard> guards[n+1];
+    guard guards[m+1];
     for (int i=1, e, c, s; i<=m; i++) {
         scanf(intf intf intf, &s, &e, &c);
-        guards[s].push_back(guard(s, e, c, i));
+        guards[i]=guard(s, e, c);
     }
+    sort(guards+1, guards+m+1);
     printf(intf, solve(guards, n, m));
 }
